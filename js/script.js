c467dae8c109f87bd673ca333cc211068cfa2b3a
danydifilippo <danydifilippo@gmail.com>
2022-10-26 18:57:54 +0200

    final

--- js/script.js
+++ js/script.js
@@ -1,9 +1,45 @@
 "use strict";
-class Calcolatore {
-    constructor(codRedd, redditoAnnuoLordo) {
+class LavoratoreTax {
+    constructor(codRedd, coeffRedd, percTasseIrpef) {
         this.codRedd = codRedd;
-        this.redditoAnnuoLordo = redditoAnnuoLordo;
+        this.coeffRedd = coeffRedd;
+        this.percTasseInps = 26;
+        this.percTasseIrpef = percTasseIrpef;
+    }
+    getUtileTasse() {
+        return this.utileTasse;
+    }
+    getTasseInps() {
+        return this.tasseInps;
+    }
+    getTasseIrpef() {
+        return this.tasseIrpef;
+    }
+    getRedditoAnnuoNetto() {
+        return this.redditoAnnuoNetto;
     }
 }
-class Commerciante extends Calcolatore {
+class Commerciante extends LavoratoreTax {
+    constructor(redditoAnnuoLordo) {
+        super(45, 40, 23);
+        this.redditoAnnuoLordo = redditoAnnuoLordo;
+    }
+    getUtileTasse() {
+        return this.redditoAnnuoLordo * 0.4;
+    }
+    getTasseInps() {
+        return this.getUtileTasse() * 0.26;
+    }
+    getTasseIrpef() {
+        return this.getUtileTasse() * 0.23;
+    }
+    getRedditoAnnuoNetto() {
+        return this.redditoAnnuoLordo - this.getTasseInps() - this.getTasseIrpef();
+    }
 }
+let comm1 = new Commerciante(40000);
+console.log(comm1);
+console.log(comm1.getUtileTasse());
+console.log(comm1.getTasseInps());
+console.log(comm1.getTasseIrpef());
+console.log(comm1.getRedditoAnnuoNetto());
--- ts/script.ts
+++ ts/script.ts
@@ -1,22 +1,63 @@
-abstract class Calcolatore {
+abstract class LavoratoreTax {
     codRedd: number
-    redditoAnnuoLordo: number
+    coeffRedd: number
+    percTasseInps: number
+    percTasseIrpef: number
     private tasseInps: number
     private tasseIrpef: number
     private utileTasse: number
-    private redditoAnnuo: number
+    private redditoAnnuoNetto: number
     
-    constructor (codRedd: number, redditoAnnuoLordo: number){
-        this.codRedd = codRedd;
-        this.redditoAnnuoLordo = redditoAnnuoLordo;
+    constructor (codRedd: number, coeffRedd: number, percTasseIrpef: number){
+        this.codRedd = codRedd
+        this.coeffRedd = coeffRedd
+        this.percTasseInps = 26
+        this.percTasseIrpef = percTasseIrpef
     }
 
-    abstract getUtileTasse: number
-    abstract getTasseInps: number
-    abstract getTasseIrpef: number
-    abstract getRedditoAnnuo: number
+    public getUtileTasse(): number {
+        return this.utileTasse
+    }
+    public getTasseInps(): number {
+        return this.tasseInps
+    }
+    public getTasseIrpef(): number{
+        return this.tasseIrpef
+    }
+    public getRedditoAnnuoNetto(): number{
+        return this.redditoAnnuoNetto
+    }
  }
 
-class Commerciante extends Calcolatore {
-    codRedd: 
-}
+class Commerciante extends LavoratoreTax {
+    redditoAnnuoLordo: number 
+
+    constructor (redditoAnnuoLordo: number){
+        super (45,40,23)
+        this.redditoAnnuoLordo= redditoAnnuoLordo
+    }
+       
+    getUtileTasse(): number {
+        return this.redditoAnnuoLordo * 0.4
+    } 
+    getTasseInps(): number {
+        return this.getUtileTasse() * 0.26
+    }
+     getTasseIrpef(): number {
+        return this.getUtileTasse() * 0.23
+     }
+     getRedditoAnnuoNetto(): number {
+        return this.redditoAnnuoLordo - this.getTasseInps() - this.getTasseIrpef()
+     }
+ }
+
+let comm1 = new Commerciante (40000)
+console.log(comm1)
+console.log(comm1.getUtileTasse());
+console.log(comm1.getTasseInps());
+console.log(comm1.getTasseIrpef());
+console.log(comm1.getRedditoAnnuoNetto());
+
+
+
+
