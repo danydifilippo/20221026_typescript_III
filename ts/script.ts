b735cb58563f5a769f9d5bfacad30d5f4249e7f4
danydifilippo <danydifilippo@gmail.com>
2022-10-27 09:13:47 +0200

    da completare

--- ts/script.ts
+++ ts/script.ts
@@ -8,24 +8,43 @@ abstract class LavoratoreTax {
     private utileTasse: number
     private redditoAnnuoNetto: number
     
-    constructor (codRedd: number, coeffRedd: number, percTasseIrpef: number){
+    constructor (codRedd: number, coeffRedd: number, percTasseIrpef: number, utileTasse: number, tasseInps: number, tasseIrpef: number, redditoAnnuoNetto: number){
         this.codRedd = codRedd
         this.coeffRedd = coeffRedd
         this.percTasseInps = 26
         this.percTasseIrpef = percTasseIrpef
+        this.utileTasse = utileTasse
+        this.tasseInps = tasseInps
+        this.tasseIrpef = tasseIrpef
+        this.redditoAnnuoNetto = redditoAnnuoNetto
     }
-
+    
     public getUtileTasse(): number {
         return this.utileTasse
     }
+    public setUtileTasse(utileTasse:number): void {
+        this.utileTasse = utileTasse
+    }
+
     public getTasseInps(): number {
         return this.tasseInps
     }
+    public setTasseInps(tasseInps:number): void {
+        this.tasseInps = this.tasseInps
+    }
+
     public getTasseIrpef(): number{
         return this.tasseIrpef
     }
+    public setTasseIrpef(tasseIrpef:number): void {
+        this.tasseIrpef = this.tasseIrpef
+    }
+
     public getRedditoAnnuoNetto(): number{
         return this.redditoAnnuoNetto
+    }
+    public setRedditoAnnuoNetto(redditoAnnuoNetto:number): void {
+        this.redditoAnnuoNetto = this.redditoAnnuoNetto
     }
  }
 
