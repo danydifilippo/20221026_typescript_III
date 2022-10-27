71e7471e8b462ee9726ad74542bb7852dc4b4400
danydifilippo <danydifilippo@gmail.com>
2022-10-27 13:52:29 +0200

    completo

--- js/script.js
+++ js/script.js
@@ -1,45 +1,176 @@
 "use strict";
 class LavoratoreTax {
-    constructor(codRedd, coeffRedd, percTasseIrpef) {
+    constructor(codRedd, coeffRedd, percTasseInps, percTasseIrpef, redditoAnnuoLordo, tasseInps, tasseIrpef) {
         this.codRedd = codRedd;
         this.coeffRedd = coeffRedd;
         this.percTasseInps = 26;
         this.percTasseIrpef = percTasseIrpef;
+        this.redditoAnnuoLordo = redditoAnnuoLordo;
+        this.tasseInps = tasseInps;
+        this.tasseIrpef = tasseIrpef;
     }
-    getUtileTasse() {
-        return this.utileTasse;
+    getCodRedd() {
+        return this.codRedd;
+    }
+    getCoeffRedd() {
+        return this.coeffRedd;
+    }
+    getPercInps() {
+        return this.percTasseInps;
+    }
+    getPercIrpef() {
+        return this.percTasseIrpef;
     }
     getTasseInps() {
         return this.tasseInps;
     }
+    setTasseInps(tasseInps) {
+        this.tasseInps = this.tasseInps;
+    }
     getTasseIrpef() {
         return this.tasseIrpef;
     }
-    getRedditoAnnuoNetto() {
-        return this.redditoAnnuoNetto;
+    setTasseIrpef(tasseIrpef) {
+        this.tasseIrpef = this.tasseIrpef;
     }
 }
 class Commerciante extends LavoratoreTax {
     constructor(redditoAnnuoLordo) {
-        super(45, 40, 23);
-        this.redditoAnnuoLordo = redditoAnnuoLordo;
+        super(45, 40, 26, 23, redditoAnnuoLordo, 0.26, 0.23);
     }
     getUtileTasse() {
         return this.redditoAnnuoLordo * 0.4;
     }
     getTasseInps() {
-        return this.getUtileTasse() * 0.26;
+        return this.getUtileTasse() * this.tasseInps;
     }
     getTasseIrpef() {
-        return this.getUtileTasse() * 0.23;
+        return this.getUtileTasse() * this.tasseIrpef;
     }
     getRedditoAnnuoNetto() {
         return this.redditoAnnuoLordo - this.getTasseInps() - this.getTasseIrpef();
     }
 }
-let comm1 = new Commerciante(40000);
-console.log(comm1);
-console.log(comm1.getUtileTasse());
-console.log(comm1.getTasseInps());
-console.log(comm1.getTasseIrpef());
-console.log(comm1.getRedditoAnnuoNetto());
+class Intermediario extends LavoratoreTax {
+    constructor(redditoAnnuoLordo) {
+        super(46, 62, 26, 25, redditoAnnuoLordo, 0.26, 0.25);
+    }
+    getUtileTasse() {
+        return this.redditoAnnuoLordo * 0.62;
+    }
+    getTasseInps() {
+        return this.getUtileTasse() * this.tasseInps;
+    }
+    getTasseIrpef() {
+        return this.getUtileTasse() * this.tasseIrpef;
+    }
+    getRedditoAnnuoNetto() {
+        return this.redditoAnnuoLordo - this.getTasseInps() - this.getTasseIrpef();
+    }
+}
+class Forfettario extends LavoratoreTax {
+    constructor(redditoAnnuoLordo) {
+        super('tutte', 78, 26, 15, redditoAnnuoLordo, 0.26, 0.15);
+    }
+    getUtileTasse() {
+        return this.redditoAnnuoLordo * 0.78;
+    }
+    getTasseInps() {
+        return this.getUtileTasse() * this.tasseInps;
+    }
+    getTasseIrpef() {
+        return this.getUtileTasse() * this.tasseIrpef;
+    }
+    getRedditoAnnuoNetto() {
+        return this.redditoAnnuoLordo - this.getTasseInps() - this.getTasseIrpef();
+    }
+}
+class Immobiliare extends LavoratoreTax {
+    constructor(redditoAnnuoLordo) {
+        super(68, 86, 26, 35, redditoAnnuoLordo, 0.26, 0.35);
+    }
+    getUtileTasse() {
+        return this.redditoAnnuoLordo * 0.86;
+    }
+    getTasseInps() {
+        return this.getUtileTasse() * this.tasseInps;
+    }
+    getTasseIrpef() {
+        return this.getUtileTasse() * this.tasseIrpef;
+    }
+    getRedditoAnnuoNetto() {
+        return this.redditoAnnuoLordo - this.getTasseInps() - this.getTasseIrpef();
+    }
+}
+class Professionisti extends LavoratoreTax {
+    constructor(redditoAnnuoLordo) {
+        super(64, 78, 26, 23, redditoAnnuoLordo, 0.26, 0.23);
+    }
+    getUtileTasse() {
+        return this.redditoAnnuoLordo * 0.78;
+    }
+    getTasseInps() {
+        return this.getUtileTasse() * this.tasseInps;
+    }
+    getTasseIrpef() {
+        return this.getUtileTasse() * this.tasseIrpef;
+    }
+    getRedditoAnnuoNetto() {
+        return this.redditoAnnuoLordo - this.getTasseInps() - this.getTasseIrpef();
+    }
+}
+class AltriLavoratori extends LavoratoreTax {
+    constructor(redditoAnnuoLordo) {
+        super('tutti', 67, 26, 25, redditoAnnuoLordo, 0.26, 0.25);
+    }
+    getUtileTasse() {
+        return this.redditoAnnuoLordo * 0.67;
+    }
+    getTasseInps() {
+        return this.getUtileTasse() * this.tasseInps;
+    }
+    getTasseIrpef() {
+        return this.getUtileTasse() * this.tasseIrpef;
+    }
+    getRedditoAnnuoNetto() {
+        return this.redditoAnnuoLordo - this.getTasseInps() - this.getTasseIrpef();
+    }
+}
+window.onload = () => {
+    let cod = document.getElementById('cod');
+    let reddito = document.getElementById('reddito');
+    let btn = document.getElementById('btn');
+    btn.addEventListener('click', function () {
+        let risultato;
+        switch (cod.value) {
+            case 'Commer':
+                risultato = new Commerciante(Number(reddito === null || reddito === void 0 ? void 0 : reddito.value));
+                break;
+            case 'Interm':
+                risultato = new Intermediario(Number(reddito === null || reddito === void 0 ? void 0 : reddito.value));
+                break;
+            case 'Immob':
+                risultato = new Immobiliare(Number(reddito === null || reddito === void 0 ? void 0 : reddito.value));
+                break;
+            case 'Profes':
+                risultato = new Professionisti(Number(reddito === null || reddito === void 0 ? void 0 : reddito.value));
+                break;
+            case 'Forfet':
+                risultato = new Forfettario(Number(reddito === null || reddito === void 0 ? void 0 : reddito.value));
+                break;
+            case 'Altro':
+                risultato = new AltriLavoratori(Number(reddito === null || reddito === void 0 ? void 0 : reddito.value));
+                break;
+        }
+        let result = document.getElementById('result');
+        result.innerHTML = `<p><strong>Cod. Lavoratore: </strong>${risultato.getCodRedd()}</p>
+        <p><strong>Coefficiente di redditività: </strong>${risultato.getCoeffRedd()}</p>
+        <p><strong>Percentuale Tasse Inps: </strong>${risultato.getPercInps()}%</p>
+        <p><strong>Percentuale Tasse Irpef: </strong>${risultato.getPercIrpef()}%</p>
+        <p><strong>Imponibile Tasse: </strong>${risultato.getUtileTasse()}</p>
+        <p><strong>Totale Tasse Inps: </strong>${risultato.getTasseInps()}</p>
+        <p><strong>Totale Tasse Irpef: </strong>${risultato.getTasseIrpef()}</p>
+        <p><strong>Reddito Annuo Netto: </strong>${risultato.getRedditoAnnuoNetto()}</p>`;
+        console.log(risultato.getPercIrpef());
+    });
+};
--- ts/script.ts
+++ ts/script.ts
@@ -3,28 +3,34 @@ abstract class LavoratoreTax {
     coeffRedd: number
     percTasseInps: number
     percTasseIrpef: number
-    private tasseInps: number
-    private tasseIrpef: number
-    private utileTasse: number
-    private redditoAnnuoNetto: number
+    redditoAnnuoLordo: number 
+    protected tasseInps: number
+    protected tasseIrpef: number
+
     
-    constructor (codRedd: number, coeffRedd: number, percTasseIrpef: number, utileTasse: number, tasseInps: number, tasseIrpef: number, redditoAnnuoNetto: number){
+    constructor (codRedd: any, coeffRedd: number, percTasseInps: number, percTasseIrpef: number, redditoAnnuoLordo: number, tasseInps: number, tasseIrpef: number){
         this.codRedd = codRedd
         this.coeffRedd = coeffRedd
         this.percTasseInps = 26
         this.percTasseIrpef = percTasseIrpef
-        this.utileTasse = utileTasse
+        this.redditoAnnuoLordo = redditoAnnuoLordo
         this.tasseInps = tasseInps
         this.tasseIrpef = tasseIrpef
-        this.redditoAnnuoNetto = redditoAnnuoNetto
     }
     
-    public getUtileTasse(): number {
-        return this.utileTasse
+    getCodRedd(){
+        return this.codRedd
     }
-    public setUtileTasse(utileTasse:number): void {
-        this.utileTasse = utileTasse
+    getCoeffRedd(){
+        return this.coeffRedd
     }
+    getPercInps(){
+        return this.percTasseInps
+    }
+    getPercIrpef(){
+        return this.percTasseIrpef
+    }
+
 
     public getTasseInps(): number {
         return this.tasseInps
@@ -40,42 +46,179 @@ abstract class LavoratoreTax {
         this.tasseIrpef = this.tasseIrpef
     }
 
-    public getRedditoAnnuoNetto(): number{
-        return this.redditoAnnuoNetto
-    }
-    public setRedditoAnnuoNetto(redditoAnnuoNetto:number): void {
-        this.redditoAnnuoNetto = this.redditoAnnuoNetto
-    }
  }
 
 class Commerciante extends LavoratoreTax {
-    redditoAnnuoLordo: number 
+    protected utileTasse?: number
+    protected redditoAnnuoNetto?: number
 
-    constructor (redditoAnnuoLordo: number){
-        super (45,40,23)
-        this.redditoAnnuoLordo= redditoAnnuoLordo
+    constructor (redditoAnnuoLordo:number){
+        super (45,40,26,23,redditoAnnuoLordo,0.26,0.23)
     }
        
     getUtileTasse(): number {
         return this.redditoAnnuoLordo * 0.4
     } 
     getTasseInps(): number {
-        return this.getUtileTasse() * 0.26
+        return this.getUtileTasse() * this.tasseInps
     }
      getTasseIrpef(): number {
-        return this.getUtileTasse() * 0.23
+        return this.getUtileTasse() * this.tasseIrpef
      }
      getRedditoAnnuoNetto(): number {
         return this.redditoAnnuoLordo - this.getTasseInps() - this.getTasseIrpef()
      }
  }
 
-let comm1 = new Commerciante (40000)
-console.log(comm1)
-console.log(comm1.getUtileTasse());
-console.log(comm1.getTasseInps());
-console.log(comm1.getTasseIrpef());
-console.log(comm1.getRedditoAnnuoNetto());
+ class Intermediario extends LavoratoreTax {
+    protected utileTasse?: number
+    protected redditoAnnuoNetto?: number
+
+    constructor (redditoAnnuoLordo:number){
+        super (46,62,26,25,redditoAnnuoLordo,0.26,0.25)
+    }
+       
+    getUtileTasse(): number {
+        return this.redditoAnnuoLordo * 0.62
+    } 
+    getTasseInps(): number {
+        return this.getUtileTasse() * this.tasseInps
+    }
+     getTasseIrpef(): number {
+        return this.getUtileTasse() * this.tasseIrpef
+     }
+     getRedditoAnnuoNetto(): number {
+        return this.redditoAnnuoLordo - this.getTasseInps() - this.getTasseIrpef()
+     }
+ }
+
+ class Forfettario extends LavoratoreTax {
+    protected utileTasse?: number
+    protected redditoAnnuoNetto?: number
+
+    constructor (redditoAnnuoLordo:number){
+        super ('tutte',78,26,15,redditoAnnuoLordo,0.26,0.15)
+    }
+       
+    getUtileTasse(): number {
+        return this.redditoAnnuoLordo * 0.78
+    } 
+    getTasseInps(): number {
+        return this.getUtileTasse() * this.tasseInps
+    }
+     getTasseIrpef(): number {
+        return this.getUtileTasse() * this.tasseIrpef
+     }
+     getRedditoAnnuoNetto(): number {
+        return this.redditoAnnuoLordo - this.getTasseInps() - this.getTasseIrpef()
+     }
+ }
+ class Immobiliare extends LavoratoreTax {
+    protected utileTasse?: number
+    protected redditoAnnuoNetto?: number
+
+    constructor (redditoAnnuoLordo:number){
+        super (68,86,26,35,redditoAnnuoLordo,0.26,0.35)
+    }
+       
+    getUtileTasse(): number {
+        return this.redditoAnnuoLordo * 0.86
+    } 
+    getTasseInps(): number {
+        return this.getUtileTasse() * this.tasseInps
+    }
+     getTasseIrpef(): number {
+        return this.getUtileTasse() * this.tasseIrpef
+     }
+     getRedditoAnnuoNetto(): number {
+        return this.redditoAnnuoLordo - this.getTasseInps() - this.getTasseIrpef()
+     }
+ }
+ class Professionisti extends LavoratoreTax {
+    protected utileTasse?: number
+    protected redditoAnnuoNetto?: number
+
+    constructor (redditoAnnuoLordo:number){
+        super (64,78,26,23,redditoAnnuoLordo,0.26,0.23)
+    }
+       
+    getUtileTasse(): number {
+        return this.redditoAnnuoLordo * 0.78
+    } 
+    getTasseInps(): number {
+        return this.getUtileTasse() * this.tasseInps
+    }
+     getTasseIrpef(): number {
+        return this.getUtileTasse() * this.tasseIrpef
+     }
+     getRedditoAnnuoNetto(): number {
+        return this.redditoAnnuoLordo - this.getTasseInps() - this.getTasseIrpef()
+     }
+ }
+ class AltriLavoratori extends LavoratoreTax {
+    protected utileTasse?: number
+    protected redditoAnnuoNetto?: number
+
+    constructor (redditoAnnuoLordo:number){
+        super ('tutti',67,26,25,redditoAnnuoLordo,0.26,0.25)
+    }
+       
+    getUtileTasse(): number {
+        return this.redditoAnnuoLordo * 0.67
+    } 
+    getTasseInps(): number {
+        return this.getUtileTasse() * this.tasseInps
+    }
+     getTasseIrpef(): number {
+        return this.getUtileTasse() * this.tasseIrpef
+     }
+     getRedditoAnnuoNetto(): number {
+        return this.redditoAnnuoLordo - this.getTasseInps() - this.getTasseIrpef()
+     }
+ }
+
+ window.onload = () => {
+    let cod = document.getElementById('cod') as HTMLSelectElement
+    let reddito = document.getElementById('reddito') as HTMLInputElement
+    let btn = document.getElementById('btn') as HTMLButtonElement
+    btn.addEventListener('click', function() {
+        let risultato: any;
+        switch (cod.value){
+        case 'Commer':
+        risultato = new Commerciante (Number(reddito?.value));
+        break;
+        case 'Interm':
+        risultato = new Intermediario (Number(reddito?.value));
+        break;
+        case 'Immob':
+        risultato = new Immobiliare (Number(reddito?.value));
+        break;
+        case 'Profes':
+        risultato = new Professionisti (Number(reddito?.value));
+        break;
+        case 'Forfet':
+        risultato = new Forfettario (Number(reddito?.value));
+        break;
+        case 'Altro':
+        risultato = new AltriLavoratori (Number(reddito?.value));
+        break;
+        }
+        let result = document.getElementById('result') as HTMLElement
+        result.innerHTML = `<p><strong>Cod. Lavoratore: </strong>${risultato.getCodRedd()}</p>
+        <p><strong>Coefficiente di redditività: </strong>${risultato.getCoeffRedd()}</p>
+        <p><strong>Percentuale Tasse Inps: </strong>${risultato.getPercInps()}%</p>
+        <p><strong>Percentuale Tasse Irpef: </strong>${risultato.getPercIrpef()}%</p>
+        <p><strong>Imponibile Tasse: </strong>${risultato.getUtileTasse()}</p>
+        <p><strong>Totale Tasse Inps: </strong>${risultato.getTasseInps()}</p>
+        <p><strong>Totale Tasse Irpef: </strong>${risultato.getTasseIrpef()}</p>
+        <p><strong>Reddito Annuo Netto: </strong>${risultato.getRedditoAnnuoNetto()}</p>`
+        console.log(risultato.getPercIrpef());
+        
+    });
+ }
+
+
+
 
 
 
